bfe370dbd1bff0d571771468b20a1dcebe9e0551
liqiang311 <385098415@qq.com>
2017-08-16 23:52:27 +0800

    template.py

---
+++ template.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def xxx
+
+
+t = []
+e = []
+for i in range(len(t)):
+    r = Solution().xxx(t[i])
+    if e[i] == r:
+        print t[i], 'done'
+    else:
+        print t[i], 'expect:', e[i], 'output:', r
